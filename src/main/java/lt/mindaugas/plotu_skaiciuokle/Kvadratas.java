48b24fa858ad22506b2770ea0c928bb747ac3092
Mindaugas <mcesnauskas@gmail.com>
2024-02-28 13:36:25 +0200

    Create sub class Kvadratas

---
+++ src/main/java/lt/mindaugas/plotu_skaiciuokle/Kvadratas.java
@@ -0,0 +1,8 @@
+package lt.mindaugas.plotu_skaiciuokle;
+
+public class Kvadratas extends Figura{
+
+    public Kvadratas(double krastine1, double krastine2) {
+        super(krastine1, krastine2);
+    }
+}
