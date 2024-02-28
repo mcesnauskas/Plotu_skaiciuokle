e581f19009a721289d8be1ebe267bddc95f73a63
Mindaugas <mcesnauskas@gmail.com>
2024-02-28 13:32:40 +0200

    Add skaiciuokPlota method to Figura class

--- src/main/java/lt/mindaugas/plotu_skaiciuokle/Figura.java
+++ src/main/java/lt/mindaugas/plotu_skaiciuokle/Figura.java
@@ -1,4 +1,30 @@
 package lt.mindaugas.plotu_skaiciuokle;
 
 public class Figura {
+
+    private double plotas;
+    private double krastine1;
+    private double krastine2;
+
+    public double getPlotas() {
+        return plotas;
+    }
+
+    public void setPlotas(double plotas) {
+        this.plotas = plotas;
+    }
+
+    public Figura(double krastine1, double krastine2) {
+        this.krastine1 = krastine1;
+        this.krastine2 = krastine2;
+    }
+
+    public void skaiciuokPlota(){
+        plotas = krastine1 * krastine2;
+    }
+
+    @Override
+    public String toString() {
+        return "Jusu figuros plotas yra: " + plotas + " kv.cm";
+    }
 }
