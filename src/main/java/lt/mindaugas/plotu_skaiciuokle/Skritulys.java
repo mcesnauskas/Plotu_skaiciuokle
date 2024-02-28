728034bf175bfb4367fac7637deff88c9064c904
Mindaugas <mcesnauskas@gmail.com>
2024-02-28 16:23:12 +0200

    Override method skaiciuokPlota in class Soindulys

--- src/main/java/lt/mindaugas/plotu_skaiciuokle/Skritulys.java
+++ src/main/java/lt/mindaugas/plotu_skaiciuokle/Skritulys.java
@@ -2,7 +2,13 @@ package lt.mindaugas.plotu_skaiciuokle;
 
 public class Skritulys extends Figura{
 
-    public Skritulys(double krastine1, double krastine2) {
-        super(krastine1, krastine2);
+    public Skritulys(double krastine1) {
+        super(krastine1, krastine1);
+    }
+
+    @Override
+    public void skaiciuokPlota() {
+        double plotas = ((getKrastine1() * getKrastine1()) * Math.PI);
+        super.setPlotas(plotas);
     }
 }
