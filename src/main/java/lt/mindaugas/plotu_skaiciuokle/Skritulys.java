1e5284a545baac1b79593d954bfc8c05f6a6bf5c
Mindaugas <mcesnauskas@gmail.com>
2024-02-28 16:06:41 +0200

    Create class Spindulys

---
+++ src/main/java/lt/mindaugas/plotu_skaiciuokle/Skritulys.java
@@ -0,0 +1,8 @@
+package lt.mindaugas.plotu_skaiciuokle;
+
+public class Skritulys extends Figura{
+
+    public Skritulys(double krastine1, double krastine2) {
+        super(krastine1, krastine2);
+    }
+}
